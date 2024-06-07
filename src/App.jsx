3b5b320b104e109dbb3f45b9fdb118bc40a03472
Lady786 <isroilovarobiyaxon786@gmail.com>
2024-06-07 22:32:05 +0100

    feat: added all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Route,  Routes } from "react-router-dom"
+import Home from "./Pages/Home"
+import Register from "./Pages/Register"
+import Login from "./Pages/Login"
+import Categories from "./Pages/Categories"
+import Products from "./Pages/Products"
 
 
 const App = () => {
   return (
-    <div className="font-bold ">App</div>
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/"  element={<Home/>} />
+      <Route path="/register"  element={<Register/>} />
+      <Route path="/login"  element={<Login/>} />
+      <Route path="/categories"  element={<Categories/>} />
+      <Route path="/products"  element={<Products/>} />
+    </Routes>
+    </BrowserRouter>
+    </>
   )
 }
 
